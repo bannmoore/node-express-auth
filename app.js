37a995e4e523f276b812b09fe686f8c707e98d0f
bannmoore <moore.brittanyann@gmail.com>
2018-09-23 19:03:19 -0400

    refactor: Create auth middleware.

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-var session = require('express-session')
-var passport = require('passport')
-var OidcStrategy = require('passport-openidconnect').Strategy
+var Auth = require('./lib/auth')
+
+var auth = new Auth()
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -22,73 +22,17 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// session setup
-app.use(
-  session({
-    secret: 'MyVoiceIsMyPassportVerifyMe',
-    resave: false,
-    saveUninitialized: true
-  })
-)
+app.use(auth.session())
 
-// passport setup
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.use(
-  'oidc',
-  new OidcStrategy(
-    {
-      issuer: process.env.OAUTH_ISSUER,
-      authorizationURL: process.env.OAUTH_AUTH_URL,
-      tokenURL: process.env.OAUTH_TOKEN_URL,
-      userInfoURL: process.env.OAUTH_USERINFO_URL,
-      clientID: process.env.OAUTH_CLIENT_ID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      callbackURL: process.env.OAUTH_CALLBACK_URL,
-      scope: 'openid profile'
-    },
-    (issuer, sub, profile, accessToken, refreshToken, done) => {
-      return done(null, profile)
-    }
-  )
-)
-
-passport.serializeUser((user, next) => {
-  next(null, user)
-})
-
-passport.deserializeUser((obj, next) => {
-  next(null, obj)
-})
+// auth routes
+app.use('/login', auth.authenticate())
+app.use('/authorization-code/callback', auth.authenticateWithRedirect())
+app.get('/logout', auth.logout())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
-// auth routes
-app.use('/login', passport.authenticate('oidc'))
-app.use(
-  '/authorization-code/callback',
-  passport.authenticate('oidc', { failureRedirect: '/error' }),
-  (req, res) => {
-    res.redirect('/')
-  }
-)
-app.get('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
-})
-
-function ensureLoggedIn (req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-
-  res.redirect('/login')
-}
-
-app.use('/profile', ensureLoggedIn, (req, res) => {
+app.use('/profile', auth.protected(), (req, res) => {
   res.render('profile', { title: 'Express', user: req.user })
 })
 
