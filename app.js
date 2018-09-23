b968853573edb367b4129a6989ac8c39b39454ca
bannmoore <moore.brittanyann@gmail.com>
2018-09-23 18:01:05 -0400

    feat: Add prettier-standard formatting.

--- app.js
+++ app.js
@@ -1,102 +1,111 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var passport = require('passport');
-var OidcStrategy = require('passport-openidconnect').Strategy;
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var session = require('express-session')
+var passport = require('passport')
+var OidcStrategy = require('passport-openidconnect').Strategy
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // session setup
-app.use(session({
-  secret: 'MyVoiceIsMyPassportVerifyMe',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: 'MyVoiceIsMyPassportVerifyMe',
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
 // passport setup
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-passport.use('oidc', new OidcStrategy({
-  issuer: process.env.OAUTH_ISSUER,
-  authorizationURL: process.env.OAUTH_AUTH_URL,
-  tokenURL: process.env.OAUTH_TOKEN_URL,
-  userInfoURL: process.env.OAUTH_USERINFO_URL,
-  clientID: process.env.OAUTH_CLIENT_ID,
-  clientSecret: process.env.OAUTH_CLIENT_SECRET,
-  callbackURL: process.env.OAUTH_CALLBACK_URL,
-  scope: 'openid profile'
-}, (issuer, sub, profile, accessToken, refreshToken, done) => {
-  return done(null, profile);
-}));
+passport.use(
+  'oidc',
+  new OidcStrategy(
+    {
+      issuer: process.env.OAUTH_ISSUER,
+      authorizationURL: process.env.OAUTH_AUTH_URL,
+      tokenURL: process.env.OAUTH_TOKEN_URL,
+      userInfoURL: process.env.OAUTH_USERINFO_URL,
+      clientID: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      callbackURL: process.env.OAUTH_CALLBACK_URL,
+      scope: 'openid profile'
+    },
+    (issuer, sub, profile, accessToken, refreshToken, done) => {
+      return done(null, profile)
+    }
+  )
+)
 
 passport.serializeUser((user, next) => {
-  next(null, user);
-});
+  next(null, user)
+})
 
 passport.deserializeUser((obj, next) => {
-  next(null, obj);
-});
+  next(null, obj)
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // auth routes
-app.use('/login', passport.authenticate('oidc'));
-app.use('/authorization-code/callback',
+app.use('/login', passport.authenticate('oidc'))
+app.use(
+  '/authorization-code/callback',
   passport.authenticate('oidc', { failureRedirect: '/error' }),
   (req, res) => {
-    res.redirect('/');
+    res.redirect('/')
   }
-);
+)
 app.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect('/');
-});
+  req.logout()
+  req.session.destroy()
+  res.redirect('/')
+})
 
-function ensureLoggedIn(req, res, next) {
+function ensureLoggedIn (req, res, next) {
   if (req.isAuthenticated()) {
-    return next();
+    return next()
   }
 
   res.redirect('/login')
 }
 
 app.use('/profile', ensureLoggedIn, (req, res) => {
-  res.render('profile', { title: 'Express', user: req.user });
-});
+  res.render('profile', { title: 'Express', user: req.user })
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
