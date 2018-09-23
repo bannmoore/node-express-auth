84de75cd68530070211267a167de8be71677eaaf
bannmoore <moore.brittanyann@gmail.com>
2018-09-23 17:47:03 -0400

    feat: Add Passport and OpenID Connect Okta authentication.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var OidcStrategy = require('passport-openidconnect').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +22,66 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// session setup
+app.use(session({
+  secret: 'MyVoiceIsMyPassportVerifyMe',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('oidc', new OidcStrategy({
+  issuer: process.env.OAUTH_ISSUER,
+  authorizationURL: process.env.OAUTH_AUTH_URL,
+  tokenURL: process.env.OAUTH_TOKEN_URL,
+  userInfoURL: process.env.OAUTH_USERINFO_URL,
+  clientID: process.env.OAUTH_CLIENT_ID,
+  clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  callbackURL: process.env.OAUTH_CALLBACK_URL,
+  scope: 'openid profile'
+}, (issuer, sub, profile, accessToken, refreshToken, done) => {
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, next) => {
+  next(null, user);
+});
+
+passport.deserializeUser((obj, next) => {
+  next(null, obj);
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// auth routes
+app.use('/login', passport.authenticate('oidc'));
+app.use('/authorization-code/callback',
+  passport.authenticate('oidc', { failureRedirect: '/error' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login')
+}
+
+app.use('/profile', ensureLoggedIn, (req, res) => {
+  res.render('profile', { title: 'Express', user: req.user });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
