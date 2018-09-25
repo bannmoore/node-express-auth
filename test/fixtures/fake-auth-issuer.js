c17ec6157cd3f0894aae5828b4f8c83b678f698d
bannmoore <moore.brittanyann@gmail.com>
2018-09-25 10:24:57 -0400

    feat: Validate access token.

--- test/fixtures/fake-auth-issuer.js
+++ test/fixtures/fake-auth-issuer.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var jwt = require('jsonwebtoken')
 
 var server = express()
 
@@ -6,10 +7,13 @@ server.get('/v1/authorize', (req, res) => {
   res.redirect(`${req.query.redirect_uri}?code=fake-code&state=${req.query.state}`)
 })
 server.post('/v1/token', (req, res) => {
-  res.send(JSON.stringify({ access_token: 'token' }))
+  res.send(JSON.stringify({ access_token: jwt.sign({}, 'shhh', { keyid: 'fake-key' }) }))
 })
 server.post('/v1/userinfo', (req, res) => {
   res.send(JSON.stringify({ name: 'Jane Doe' }))
+})
+server.get('/v1/keys', (req, res) => {
+  res.send(JSON.stringify({ keys: [{ kid: 'fake-key' }]}))
 })
 
 module.exports = server
